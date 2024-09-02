4d5b336cb9d1648493789981b7c97fb05fef70bf
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-09-02 23:00:36 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,21 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
-        mercedes.accelerate(20);
-        mercedes.accelerate(-60);
+        Vehicle vehicle = new Vehicle("Generic Vehicle");
+        Car car = new Car("Sedan", 4, 4, 5, true, "Luxury", 5);
+        F1 f1 = new F1("Ferrari", true);
+
+        // Using Vehicle
+        vehicle.move(60, 90);
+        vehicle.steer(30);
+        vehicle.stop();
+
+        // Using Car
+        car.changeGear(3);
+        car.changeSpeed(80, 45);
+
+        // Using F1
+        f1.accelerate(70);
+        f1.accelerate(-50);
     }
 }
