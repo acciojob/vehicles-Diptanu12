99963e73fafcb66b8128c4455cee300b0a774080
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-09-04 23:49:45 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -2,35 +2,35 @@ package com.driver;
 
 public class Car extends Vehicle {
     private int wheels;
-    private String type;
     private int doors;
     private int gears;
     private boolean isManual;
     private int currentGear;
+    private String type;
     private int seats;
 
+    // Updated constructor
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         super(name);
         this.wheels = wheels;
         this.doors = doors;
         this.gears = gears;
         this.isManual = isManual;
+        this.currentGear = 1; // Default gear is 1
         this.type = type;
         this.seats = seats;
-        this.currentGear = 1;
     }
 
-    public void changeGear(int newGear){
-        this.currentGear = newGear;
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
-    }
-
-    public void changeSpeed(int newSpeed, int newDirection){
-        move(newSpeed, newDirection);
-        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    public void changeGear(int gear) {
+        this.currentGear = gear;
     }
 
     public int getCurrentGear() {
         return currentGear;
+    }
+
+    // Updated method signature to match usage
+    public void changeSpeed(int speed) {
+        super.changeSpeed(speed, getCurrentDirection()); // Calls the superclass method
     }
 }
